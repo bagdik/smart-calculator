66ac3be500e57b4eb4835b168935f95cdea869c6
bagdik <bahdanchik@tut.by>
2018-03-05 21:12:50 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,27 +1,78 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
+  constructor(initValue) {
+    this.init = initValue;
+    this.operations = [];
   }
+  get result() {
+    let rpn = [this.init]; // reverse polish notation
+    let ops = [];
 
+    this.operations.forEach(op => { 
+      while (ops.length && (ops[ops.length - 1]).priority <= op.priority) {
+        rpn.push(ops.pop().op);
+      }
+      ops.push(op);
+      rpn.push(op.val);
+    });
+    while (ops.length) {
+      rpn.push(ops.pop().op)
+    }
+    let res = [];
+    rpn.forEach(token => {
+      if (typeof token === 'function') {
+        res.push(token(res.pop(), res.pop()));
+      } else {
+        res.push(token);
+      }
+    });
+    return res.pop();
+  }
   add(number) {
-    // your implementation
+    this.operations.push({
+        op : (a, b) => a + b,
+        priority : 2,
+        val : number
+    });
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.operations.push({
+        op : (a, b) => b - a,
+        priority : 2,
+        val : number
+    });
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.operations.push({
+        op : (a, b) => a * b,
+        priority : 1,
+        val : number
+    });
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.operations.push({
+        op : (a, b) => b / a,
+        priority : 1,
+        val : number
+    });
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.operations.push({
+        op : (a, b) => Math.pow(b, a),
+        priority : 0,
+        val : number
+    });
+    return this;
+  }
+  valueOf(){
+      return this.result;
   }
 }
-
 module.exports = SmartCalculator;
